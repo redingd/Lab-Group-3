e170aed8bf8b7536a3153cbdbce8bd6a667c4d21
redingd <131977287+redingd@users.noreply.github.com>
2025-09-21 22:35:43 -0400

    Update extract_csv.py

--- group_3_lab3/extract_csv.py
+++ group_3_lab3/extract_csv.py
@@ -1,6 +1,6 @@
 import csv
 
-# denote which columns will be extracted from the original csv file, as listed in the header
+# denote which columns will be extracted from the input csv file, as listed in the header
 columns_to_extract = ['organizationid', 'GradeLevel', 'Test', 'Orglevel', 'organizationname', 'SchoolYear']
 
 # open input and output csv files
@@ -11,10 +11,11 @@ with (open('./data/raw/20222023.csv', 'r', newline = '', encoding = 'utf-8') as 
     reader = csv.DictReader(infile)
     writer = csv.DictWriter(outfile, fieldnames = columns_to_extract)
 
-    # write header to output
+    # write header to output file
     writer.writeheader()
 
-    # print previously denoted columns (see line 4) from original file to new one
+    # print previously denoted columns (see line 4) from input file to output file
     for row in reader:
         filtered_row = {key: row[key] for key in columns_to_extract}
+
         writer.writerow(filtered_row)
