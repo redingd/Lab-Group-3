23ca5495e44cdf53340edf48aba56c26323fbb01
mcktol7 <mckinleytolley7@gmail.com>
2025-10-06 22:35:00 -0400

    Update group_3_Lab5.py
    Reads pdf file, turns all text into a nested list where each list in the list is usually one row.

--- Lab_5/group_3_Lab5.py
+++ Lab_5/group_3_Lab5.py
@@ -1,1 +1,22 @@
+import pdfplumber
+pdf_filename = "Table9.pdf"
 
+with pdfplumber.open(pdf_filename) as pdf:
+    pages = pdf.pages
+    # string to hold raw data from pdf, turns into nested list later
+    raw_pdf_data = ""
+
+    # adding data to string
+    i = 0
+    while i < 6:
+        raw_pdf_data += (pages[i].extract_text())
+        i += 1
+
+    # turning string into one big list
+    list_raw_data = raw_pdf_data.split("\n")
+
+    # turning big list into nested list, where each row is mostly one line for actual data
+    nested_list_raw_data = []
+    for line in list_raw_data:
+        temp_row = [line]
+        nested_list_raw_data.append(temp_row)
