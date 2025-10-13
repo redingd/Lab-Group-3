c6f1c415c5d810d5f1286561c2dd3682709ca579
redingd <131977287+redingd@users.noreply.github.com>
2025-10-12 23:01:56 -0400

    Update group_3_Lab5.py

--- Lab_5/group_3_Lab5.py
+++ Lab_5/group_3_Lab5.py
@@ -20,3 +20,46 @@ with pdfplumber.open(pdf_filename) as pdf:
     for line in list_raw_data:
         temp_row = [line.split(" ")]
         nested_list_raw_data.extend(temp_row)
+    nested_list_cleaned_data = []
+    for line in nested_list_raw_data:
+        i = 0
+        while i < len(line):
+            if (line[i] == "-" or line[i] == "x" or line[i] == "y" or line[i] == "v" or line[i] == "x,y" or line[i] == "v,x,y" or line[i] == "**"):
+                line.pop(i)
+                
+                temp_row_data = []
+                if (i == 2):
+                    temp_row_data.extend(line[1], "Child labour (%) 2005-2012", line [i])
+                    nested_list_cleaned_data.append(temp_row_data)
+                if (i == 5):
+                    temp_row_data.extend(line[1], "Child marriage (%) 2005-2012_married by 15", line [i])
+                    nested_list_cleaned_data.append(temp_row_data)
+                if (i == 6):
+                    temp_row_data.extend(line[1], "Child marriage (%) 2005-2012_married by 18", line [i])
+                    nested_list_cleaned_data.append(temp_row_data)
+                if (i == 7):
+                    temp_row_data.extend(line[1], "Birth registration (%) 2005-2012", line [i])
+                    nested_list_cleaned_data.append(temp_row_data)
+                if (i == 8):
+                    temp_row_data.extend(line[1], "Female genital mutilation/cutting (%) 2002-2012_prevalence_women", line [i])
+                    nested_list_cleaned_data.append(temp_row_data)
+                if (i == 9):
+                    temp_row_data.extend(line[1], "Female genital mutilation/cutting (%) 2002-2012_prevalence_girls", line [i])
+                    nested_list_cleaned_data.append(temp_row_data)
+                if (i == 10):
+                    temp_row_data.extend(line[1], "Female genital mutilation/cutting (%) 2002-2012_attitudes", line [i])
+                    nested_list_cleaned_data.append(temp_row_data)
+                if (i == 11):
+                    temp_row_data.extend(line[1], "Justification of wife beating (%) 2005-2012_male", line [i])
+                    nested_list_cleaned_data.append(temp_row_data)
+                if (i == 12):
+                    temp_row_data.extend(line[1], "Justification of wife beating (%) 2005-2012_female", line [i])
+                    nested_list_cleaned_data.append(temp_row_data)
+                if (i == 13):
+                    temp_row_data.extend(line[1], "Violent Discipline (%) 2005-2012", line [i])
+                    nested_list_cleaned_data.append(temp_row_data)
+
+
+with open("..\Group_3_Lab5\group_3_Lab5.csv", "w", newline="") as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerows(nested_list_cleaned_data)
