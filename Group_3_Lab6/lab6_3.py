b41d768a63a09a12a326b699e2c8c6dda9801314
redingd <131977287+redingd@users.noreply.github.com>
2025-10-16 14:41:47 -0400

    Update lab6_3.py

--- Group_3_Lab6/lab6_3.py
+++ Group_3_Lab6/lab6_3.py
@@ -2,3 +2,7 @@
 # Menu Interface: Create a simple menu in the script that allows users to:
 # Select either Part 1 (Data Merging) or Part 2 (Data Cleaning).
 # Run the selected function.
+
+# for part 2, there are two csv files needed which are both in a zip file linked in the assignment instructions on canvas
+# One of the files is too large to import into GitHub, so only one of them is currently in the folder for this lab
+# Whoever submits the assignment will need to make sure that they include the other csv file that is 220 MB
