3ded91442e2a8005fce88891ee034c9cbcffac81
redingd <131977287+redingd@users.noreply.github.com>
2025-10-16 14:55:29 -0400

    Add files via upload

---
+++ lab6_3.py
@@ -0,0 +1,11 @@
+# Submit a Single Python Script: The script should include both functions for Parts 1 and 2.
+# Menu Interface: Create a simple menu in the script that allows users to:
+# Select either Part 1 (Data Merging) or Part 2 (Data Cleaning).
+# Run the selected function.
+
+# All xlsx files are for part 1, all csv files are for part 2
+
+# for part 2, there are two csv files needed which are both in a zip file linked in the assignment instructions on canvas
+# One of the files is too large to import into GitHub, so only one of them is currently in the folder for this lab
+# Whoever submits the assignment will need to make sure that they include the other csv file that is 220 MB
+
