9e8b94ba5e9e08df36648df5e5b178403b5d2d3a
redingd <131977287+redingd@users.noreply.github.com>
2025-10-16 14:30:23 -0400

    Create lab6_3.py

---
+++ Group_3_Lab6/lab6_3.py
@@ -0,0 +1,4 @@
+# Submit a Single Python Script: The script should include both functions for Parts 1 and 2.
+# Menu Interface: Create a simple menu in the script that allows users to:
+# Select either Part 1 (Data Merging) or Part 2 (Data Cleaning).
+# Run the selected function.
