e47c45b26bc16924a4b7d106d345af3e8eaa9475
redingd <131977287+redingd@users.noreply.github.com>
2025-10-16 14:47:51 -0400

    Update lab6_3.py

--- Group_3_Lab6/lab6_3.py
+++ Group_3_Lab6/lab6_3.py
@@ -3,6 +3,8 @@
 # Select either Part 1 (Data Merging) or Part 2 (Data Cleaning).
 # Run the selected function.
 
+# All xlsx files are for part 1, all csv files are for part 2
+
 # for part 2, there are two csv files needed which are both in a zip file linked in the assignment instructions on canvas
 # One of the files is too large to import into GitHub, so only one of them is currently in the folder for this lab
 # Whoever submits the assignment will need to make sure that they include the other csv file that is 220 MB
